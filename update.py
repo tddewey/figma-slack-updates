d1ab4124f8ea05d2f5b0f84bdcf888353ac4dfec
Taylor Dewey <td@tddewey.com>
2022-03-10 19:48:08 -0800

    Fixed the date comparison in the lambda function

--- update.py
+++ update.py
@@ -17,17 +17,20 @@ def get_updates():
   print(data)
   versions = data["versions"]
 
-  # Filter out versions that were created more than 24 hours ago, or where the description is empty
-  filter_function = lambda x: maya.parse(datetime.utcnow() - x['created_at'].datetime().datetime() < timedelta(hours=24) and x['description'] is not None and len(x['description']) > 0)
-  todays_versions = list(filter(filter_function, versions))
+    now = time.time()
+    hours_in_seconds = 1 * 60 * 60
+
+    # Filter out versions that were created more than 24 hours ago, or where the description is empty
+    filter_function = lambda x: now - unix_time_from_iso8601(x['created_at']) < hours_in_seconds*24 and x['description'] is not None and len(x['description']) > 0
+    todays_versions = list(filter(filter_function, versions))
+    
   if len(todays_versions) > 0:
     message = format_message(todays_versions)
     post_message(message)
 
 def format_message(todays_versions):
   date = datetime.datetime.today()
-  message = "Design library updates: \n\n"
-  # message = "str(date.year) + "/" + str(date.month) + "/" + str(date.year) + "\n"
+  message = "*Design library updates:* \n\n"
 
   for version in todays_versions:
     created_at = version["created_at"]
@@ -36,7 +39,7 @@ def format_message(todays_versions):
     user = version["user"]["handle"]
 
     message += "*" + label + "*\n"
-    message += "_<!date^" + unix_time_from_iso8601(created_at) + "^{time} {date_short_pretty}|" + created_at + "> by " + user + "_\n"
+    message += "_<!date^" + str(unix_time_from_iso8601(created_at)) + "^{time} {date_short_pretty}|" + created_at + "> by " + user + "_\n"
     message += description + "\n"
     message += "\n-----------\n\n"
 
@@ -45,7 +48,7 @@ def format_message(todays_versions):
 def unix_time_from_iso8601(t):
     parsed_t = dp.parse(t)
     t_in_seconds = parsed_t.strftime('%s')
-    return t_in_seconds
+    return float(t_in_seconds)
 
 def post_message(message):
   SLACK_TEAM_ID = environ.get('SLACK_TEAM_ID')
